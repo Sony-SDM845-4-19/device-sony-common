28466f4ea9e25a8ddc44aff5e9bd08da90266be5
Pavel Dubrova <pashadubrova@gmail.com>
2025-06-22 10:25:23 +0300

    odm: Build our own ODM image
    We will use our set of blobs. Also switch version to v1.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- common-odm.mk
+++ common-odm.mk
@@ -1,7 +1,5 @@
 # Vendor version
-TARGET_VENDOR_VERSION := v7
-
-ifneq (,$(SONY_BUILD_ODM))
+TARGET_VENDOR_VERSION := v1
 
 PRODUCT_VENDOR_MOVE_ENABLED := true
 
@@ -14,6 +12,8 @@ BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
 
 PRODUCT_ODM_PROPERTIES += \
     ro.odm.version=$(PLATFORM_VERSION)_$(SOMC_KERNEL_VERSION)_$(SOMC_PLATFORM)_$(TARGET_VENDOR_VERSION)
+
+ifneq (,$(SONY_BUILD_ODM))
 
 # Clear device-specific info from platform-level odm image
 PRODUCT_PACKAGES += \
